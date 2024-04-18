d295620828e3d3c50b0d58fcf0a881fc78592308
hacker4516 <kenan.tas@elev.ga.ntig.se>
2024-04-19 01:52:15 +0200

    lite lines

--- src/Anvandarsystem.java
+++ src/Anvandarsystem.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Anvandarsystem {
     public static void main(String[] args) {
+        Scanner fortnite = new Scanner(System.in);
         System.out.println("Hej\nLogga in nedan:");
         System.out.println("Användarnamn: ");
-
 
     }
 }
