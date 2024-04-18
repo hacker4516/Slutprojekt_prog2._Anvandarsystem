c2b1d917b8860aad852e0bf3e93f5c058d663e01
hacker4516 <kenan.tas@elev.ga.ntig.se>
2024-04-19 00:12:31 +0200

    lite lines

---
+++ src/Anvandarsystem.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Anvandarsystem {
+    public static void main(String[] args) {
+        System.out.println("Hej\nLogga in nedan:");
+        System.out.println("Användarnamn: ");
+
+
+    }
+}
