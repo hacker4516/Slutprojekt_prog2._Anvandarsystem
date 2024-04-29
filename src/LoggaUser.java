17c97c941db9af833adc69fb350d8a091067b8c5
hacker4516 <kenan.tas@elev.ga.ntig.se>
2024-04-29 06:18:40 +0200

    boop

--- src/LoggaUser.java
+++ src/LoggaUser.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class LoggaUser { //används för att logga in, förstod inte varför det inte funkade i AnvandarSystem som SkapaUser gjorde
-
+//vad LoggaUser ger möjligheten till är att användaren kan sedan utföra olika saker, som till exempel köpa något osv
+    //användare med admins roll kan även se hur många användare det finns samt namnen, och har möjligheten att ta bort användare också (egenskaper)
     static String UserNamn;
     static String UserPass;
     static boolean loggadin;
